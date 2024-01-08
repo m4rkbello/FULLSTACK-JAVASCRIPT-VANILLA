4a59324a72d17459216b0d02dc651fd560ca915a
m4rkbello <markbello10041999@gmail.com>
2024-01-08 23:42:19 +0800

    ADD

--- 015 - String Methods (chatAt, concat, includes, indexOf)/index.js
+++ 015 - String Methods (chatAt, concat, includes, indexOf)/index.js
@@ -15,7 +15,6 @@ let search = "SALIG KO SA GINOO!";
 //if true ang result? pasabot na nakita niya ang ang variable kay present ang argument
 console.log("INCLUDES METHOD", datas.includes(search));
 
-
 //INDEXOF - IHAPON NIYA ANG LETTER SUGOD SA [0] UNSAY SUGOD SA ARGUMENT NA LETTER: SALIG KO SA GINOO 
 let padayon = "KAYA NAKO! NAGA SALIG SA GINOO! NAGA SALIG KO SA AKUNG CAPASIDAD MAONG NAA KO DIRI!";
 //initializer niya 
@@ -23,7 +22,6 @@ let argument = "L";
 //if true ang result? pasabot na nakita niya ang ang variable kay present ang argument
 // console.log("INDEXOF METHOD", padayon.indexOf(argument));
 //18 ang result
-
 
 //LASTINDEXOF - IHAPON NIYA ANG LAST LETTER SUGOD SA C GAMIT REVERSE INDEX[0] NA REVERSE UNSAY LAST SA ARGUMENT NA LETTER: SALIG KO SA GINOO 
 let padayonlang = "KAYA NAKO! NAGA SALIG SA GINOO! W NAGA SALIG KO SA AKUNG CAPASIDAD MAONG NAA KO DIRI!";
